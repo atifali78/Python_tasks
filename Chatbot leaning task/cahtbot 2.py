163f088c017b541a472620116b782ab06728c560
Atif Ali <atif7803320@gmail.com>
2023-10-04 23:59:13 -0700

    add time in chatbot

---
+++ Chatbot leaning task/cahtbot 2.py
@@ -0,0 +1,22 @@
+import time
+
+qna = {
+    "hi": "HEY",
+    "what is your name": "My name is Chatbot",
+    "how are you": "I am fine",
+    "how old are you": "I AM VIRTUAL MACHINE",
+    
+}
+
+while True:
+    qs = input()
+
+    if qs == "quit":
+        break
+     elif qs == "time":
+        current_time = time.ctime()
+        print("The current time is:", current_time)
+    elif qs in qna:
+        print(qna[qs])
+    else:
+        print("I don't have an answer for that question.")
